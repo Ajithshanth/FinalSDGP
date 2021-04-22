d6d2422919d3d7e73d929aedccf0af052a591e72
Ajithshanth <ajithshanth.m@gmail.com>
2021-04-23 00:44:13 +0530

    Final  Commit

--- HouseValuePrediction/HouseValuePrediction/views.py
+++ HouseValuePrediction/HouseValuePrediction/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import ensemble
 import joblib
 
 getModel = joblib.load('./models/finalized_model.sav')
